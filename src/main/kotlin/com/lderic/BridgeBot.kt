7bf7dead71b74a7899719ebcdfc3bdeb191af06a
eric <lderic0415@gmail.com>
2022-04-08 19:45:20 -0700

    Add working dir

--- src/main/kotlin/com/lderic/BridgeBot.kt
+++ src/main/kotlin/com/lderic/BridgeBot.kt
@@ -1,12 +1,21 @@
 package com.lderic
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.BotFactory
+import java.io.File
+import kotlin.coroutines.EmptyCoroutineContext
 
-object BridgeBot {
-    val bot: Bot = BotFactory.newBot(3283056961, "")
+object BridgeBot : Thread("bot-main") {
+    private val bot: Bot = BotFactory.newBot(3283056961, "") {
+        this.workingDir = File(System.getProperty("user.dir") + "/temp")
+    }
+    private val botCoroutineScope = CoroutineScope(EmptyCoroutineContext)
 
-    suspend fun init() {
-        bot.getFriend(1278350812)!!.sendMessage("Bot has been initialized")
+    override fun run() {
+        botCoroutineScope.launch {
+            bot.getFriend(1278350812)!!.sendMessage("Bot has been initialized")
+        }
     }
 }
