14ec79eb4792737680dd0e6edfc9dab1e9125ea7
eric <lderic0415@gmail.com>
2022-04-10 08:44:30 -0700

    Create ServerPlayerEntityMixin

---
+++ src/main/java/com/lderic/mixins/ServerPlayerEntityMixin.java
@@ -0,0 +1,20 @@
+package com.lderic.mixins;
+
+import com.lderic.Mod;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+    @Inject(method = "onDeath", at = @At("RETURN"))
+    public void playerDeathPosition(DamageSource damageSource, CallbackInfo ci){
+        ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        if (player != null) {
+
+        }
+    }
+}
